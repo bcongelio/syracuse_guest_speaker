be13623a79ae04fbff70515225e20e6c48afea1e
Congelio <congelio@kutztown.edu>
2024-10-07 10:02:35 -0400

    small changes

--- viz_coding.R
+++ viz_coding.R
@@ -13,7 +13,8 @@ team_airyards <- pbp |>
   filter(complete_pass == 1 & !is.na(receiver) & !is.na(air_yards) & !is.na(down)) |> 
   group_by(posteam, air_yards) |> 
   summarize(
-    total = n())
+    total = n()) |> 
+  filter(air_yards >= 0)
 
 team_airyards <- team_airyards |> 
   left_join(teams, by = c("posteam" = "team_abbr"))
@@ -24,7 +25,7 @@ meansd <- function(x, ...) {
   c(mean - sd, mean, mean + sd)
 }
 
-ggplot(team_airyards, aes(x = air_yards, y = reorder(posteam, -air_yards),
+ggplot(team_airyards, aes(x = air_yards, y = reorder(posteam, air_yards),
                           fill = posteam, color = posteam)) +
   geom_density_ridges(quantile_lines = TRUE,
                       quantile_fun = meansd) +
@@ -41,5 +42,5 @@ ggplot(team_airyards, aes(x = air_yards, y = reorder(posteam, -air_yards),
         strip.background = element_rect(fill = "#f7f7f7"),
         strip.text = element_text(family = "Roboto Condensed",
                                   face = "bold",
-                                  size = 12)) +
+                                  size = 12.5)) +
   nfl_analytics_theme()
